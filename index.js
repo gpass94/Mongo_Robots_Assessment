75803c32b7b7ba796233d1804e082bf10a55e8a3
Grant Paschal <gpass94@gmail.com>
2017-06-20 16:02:44 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const path = require('path');
+const app = express();
+const userData = require('./data.js');
+
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(express.static('public'))
+
+
+app.get('/users', function(req, res){
+  res.render('index', userData)
+})
+
+
+app.listen(3000, function () {
+  console.log("App is running on local host:3000");
+});
